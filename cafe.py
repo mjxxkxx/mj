144b420d4757613bc90a236d50767a80c4d78bbe
MJ <mjinkim17@gmail.com>
2024-07-26 09:51:27 +0900

    Update cafe.py

--- cafe.py
+++ cafe.py
@@ -7,11 +7,11 @@ while True:
             change = money_input - 300
             print(f"거스름돈 {change}를 주고 커피를 줍니다.")
             remain -= 1  # 커피 판매 후 남은 커피 수량 감소
-            if remain == 0:
-                print("커피가 다 떨어졌습니다. 판매를 중지합니다.")
-                break
+           
         else:
             print("돈을 다시 돌려주고 커피를 주지 않습니다.")
             print(f"남은 커피의 양은 {remain}입니다.")
-        
-        
+    
+    elif remain == 0:
+            print("커피가 다 떨어졌습니다. 판매를 중지합니다.")
+            break
