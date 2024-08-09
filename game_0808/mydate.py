ccfa554bcad074394de15689a552c3be5925e8a9
MJ <mjinkim17@gmail.com>
2024-08-09 17:41:10 +0900

    update

--- game_0808/mydate.py
+++ game_0808/mydate.py
@@ -19,9 +19,6 @@ class MyDate:
             else:
                 return 0 <= day <= 30
 
-        check_valid_day(2022, 2, 15)
-        check_valid_day(2022, 4, 60)
-        
         assert check_valid_day(year, month, day)
         self.day = day
 
@@ -34,8 +31,54 @@ class MyDate:
         assert 0 <= sec <= 60
         self.sec = sec
 
+    def days_in_month(self, month, year):
+        """ Returns the number of days in a given month and year """
+        if month == 2:
+            # Leap year logic
+            if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+                return 29  # Leap year
+            else:
+                return 28  # Non-leap year
+        elif month in [1, 3, 5, 7, 8, 10, 12]:
+            return 31
+        else:
+            return 30
+
     def __add__(self, other):
-    
+
+        remainder_sec = (self.sec + other.sec) % 60
+        quotient_sec = (self.sec + other.sec) // 60
+
+        remainder_minute = (self.minute + other.minute + quotient_sec) % 60
+        quotient_minute = (self.minute + other.minute + quotient_sec) // 60
+
+        remainder_hour = (self.hour + other.hour + quotient_minute) % 24
+        quotient_hour = (self.hour + other.hour + quotient_minute) // 24
+
+        total_days = self.day + other.day + quotient_hour
+
+        month = self.month
+        year = self.year + other.year
+
+        while total_days > self.days_in_month(month, year):
+            total_days -= self.days_in_month(month, year)
+            month += 1
+            if month > 12:
+                month = 1
+                year += 1
+
+        remainder_day = total_days
+
+        return MyDate(
+            year=year,
+            month=month,
+            day=remainder_day,
+            hour=remainder_hour,
+            minute=remainder_minute,
+            sec=remainder_sec
+        )
+
+        '''
         remainder_sec = (self.sec + other.sec) % 60
         quotient_sec = (self.sec + other.sec) // 60
 
@@ -69,14 +112,9 @@ class MyDate:
             quotient_day = (self.day + other.day + quotient_hour) // 30
             
         month = remainder_month + quotient_day
-
-        if month > 12:
-            remainder_month = month % 12
-            quotient_month = month // 12
-            month = remainder_month
-            year += quotient_month
-
-        return MyDate(year = year, month = month, day = remainder_day, hour = remainder_hour, minute = remainder_minute, sec = remainder_sec)          
+     
+        return MyDate(year = year, month = remainder_month, day = remainder_day, hour = remainder_hour, minute = remainder_minute, sec = remainder_sec)
+        '''          
 
 
     def __sub__(self, other):
@@ -100,12 +138,21 @@ class MyDate:
                 assert False, f'{r} is larger than {l}'
             
             for idx, elem in enumerate(res[first_nonzero_idx:-1], start = first_nonzero_idx):
-                if res[idx+1] < 0: # idx에서 idx + 1로 받아내림을 실행 
-                    res[idx] = res[idx] - 1 
-                    res[idx+1] = table[idx] + res[idx+1]
+                if idx < 2:
+                    if res[idx+1] <= 0: # idx에서 idx + 1로 받아내림을 실행 
+                        res[idx] = res[idx] - 1 
+                        res[idx+1] = table[idx] + res[idx+1]
+                else:
+                    if res[idx+1] < 0: # idx에서 idx + 1로 받아내림을 실행 
+                        res[idx] = res[idx] - 1 
+                        res[idx+1] = table[idx] + res[idx+1]
             return res 
 
-        res = list_sub(self, other, table = {0:12, 1:31, 2:24, 3:60, 4:60})
+        self_list = [self.year, self.month, self.day, self.hour, self.minute, self.sec]
+        other_list =[other.year, other.month, other.day, other.hour, other.minute, other.sec]
+
+        res = list_sub(self_list, other_list, table = {0:12, 1:31, 2:24, 3:60, 4:60})
+        # print(self_list, other_list, [a-b for a, b in zip(self_list, other_list)], res)
         # return MyDate(res[0], res[1], res[2], res[3], res[4], res[5])
         return MyDate(*res)
 
@@ -159,6 +206,10 @@ class MyDate:
                                 minute = self.minute - other.minute, 
                                 sec = self.sec - other.sec)
         '''
+
+    def __str__(self):
+        return f'{self.year}/{self.month}/{self.day} {self.hour}:{self.minute}:{self.sec}'
+
     def __eq__(self, other):
         if self.year == other.year \
                 and self.month == other.month \
@@ -169,7 +220,19 @@ class MyDate:
             return True
         else: return False
 
+
+
     def __lt__(self, other):
+        def lst_cmp(l, r):
+            for a, b in zip(l, r):
+                if a < b:
+                    return True 
+            else:
+                return False 
+        self_list = [self.year, self.month, self.day, self.hour, self.minute, self.sec]
+        other_list =[other.year, other.month, other.day, other.hour, other.minute, other.sec]
+
+        return lst_cmp(self_list, other_list)
         if self.year < other.year :
             if self.month < other.month:
                 if self.day < other.day:
@@ -185,6 +248,8 @@ class MyDate:
         else: return False
     
     def __le__(self, other):
+        return self == other or self < other 
+
         if self.year <= other.year :
             if self.month <= other.month:
                 if self.day <= other.day:
@@ -200,6 +265,7 @@ class MyDate:
         else: return False
 
     def __gt__(self, other):
+        return not self == other and not self < other
         if self.year >= other.year :
             if self.month >= other.month:
                 if self.day >= other.day:
@@ -215,6 +281,7 @@ class MyDate:
         else: return False
 
     def __ge__(self, other):
+        return self == other or not self < other 
         if self.year >= other.year :
             if self.month >= other.month:
                 if self.day >= other.day:
@@ -239,9 +306,14 @@ if __name__ == '__main__':
     assert d1 + d3 == MyDate(2022, 4, 2, 14, 30)
     print(d1-d3)
     assert d1 - d3 == MyDate(2022, 3, 31, 14, 30) 
-    d4 = MyDate(2022, 3, 31, 14, 30)
 
-    assert d3 + d4 == MyDate(2022, 4, 1, 14, 30)
+    d4 = MyDate(2022, 1, 31)
+    d5 = MyDate(day = 30)
+
+    assert d4 + d5 == MyDate(2022, 3, 2)
+    # d4 = MyDate(2022, 3, 31, 14, 30)
+
+    # assert d3 + d4 == MyDate(2022, 4, 1, 14, 30)
 
     # assert d1 < d2 
 
